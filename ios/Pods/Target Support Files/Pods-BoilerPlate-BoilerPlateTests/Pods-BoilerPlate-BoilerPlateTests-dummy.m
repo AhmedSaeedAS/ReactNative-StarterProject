20dad5e07b3d56d578fa5b14f35162167ca30903
Ahmed <muhammadahmedsaeed@gmail.com>
2020-04-05 17:45:39 +0500

    initial commit

---
+++ ios/Pods/Target Support Files/Pods-BoilerPlate-BoilerPlateTests/Pods-BoilerPlate-BoilerPlateTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BoilerPlate_BoilerPlateTests : NSObject
+@end
+@implementation PodsDummy_Pods_BoilerPlate_BoilerPlateTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-BoilerPlate/Pods-BoilerPlate-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BoilerPlate : NSObject
+@end
+@implementation PodsDummy_Pods_BoilerPlate
+@end
